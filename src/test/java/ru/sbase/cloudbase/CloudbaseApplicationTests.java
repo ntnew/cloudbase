0a20cbfaf2a2f0ebe01d5bcd725fde900719038e
Timur Kutateladze <Timur.Kutateladze@waveaccess.ru>
2023-10-30 23:15:19 +0400

    adding files

---
+++ src/test/java/ru/sbase/cloudbase/CloudbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sbase.cloudbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
